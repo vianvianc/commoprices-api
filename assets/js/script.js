b7e7143b3de4a1ba19ada266280085d11f2dfc30
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2023-01-12 23:36:38 +0300

    updates script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,11 @@
-var commodityInputEl = document.querySelector("#commodity");
-var commodityContainerEl = document.querySelector("#commodity-container");
-var commoditySearchTerm = document.querySelector("#commodity-search-term");
+
+var searchForm = document.querySelector("#search-form");
+var searchTermInput = document.querySelector("#commodity");
+var formatSelect = document.querySelector("#search-format");
 
 apiKey = "a45j8kri99ls4lwqgp45o8358l4y6nfqv012ujr3p312cveb9kv8x40207q1";
 
-var formSubmitHandler = function (event) {
-  event.preventDefault();
-  var commodityName = commodityInputEl.value.trim();
-  if (commodityName) {
-    getCommodity(commodityName);
-    commodityContainerEl.textContent = "";
-    commodityInputEl.value = "";
-  } else {
-    ("Please enter a valid commodity symbol");
-  }
-};
+
 
 var getCommodity = function () {
   var apiUrl =
@@ -39,3 +30,4 @@ var getCommodity = function () {
     });
 };
 getCommodity();
+

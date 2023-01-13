c0a4cd34e4e22eee78cfdc2a2a048ac1d588a160
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2023-01-13 03:51:02 +0300

    updates script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,15 @@
-
 var searchForm = document.querySelector("#search-form");
 var searchTermInput = document.querySelector("#commodity");
 var formatSelect = document.querySelector("#search-format");
+var commodityContainer = document.querySelector("#commodity-container");
 
 apiKey = "a45j8kri99ls4lwqgp45o8358l4y6nfqv012ujr3p312cveb9kv8x40207q1";
-
-
 
 var getCommodity = function () {
   var apiUrl =
     "https://commodities-api.com/api/latest?access_key=" +
     apiKey +
-    "&base=USD&symbols=XAU,XAG";
+    "&base=USD&symbols=BRENTOIL";
 
   fetch(apiUrl)
     .then(function (response) {
@@ -19,6 +17,7 @@ var getCommodity = function () {
         console.log(response);
         response.json().then(function (data) {
           console.log(data);
+          renderResults(data);
           //   displayRepos(data, user);
         });
       } else {
@@ -29,5 +28,11 @@ var getCommodity = function () {
       alert("Unable to connect to Commodities API");
     });
 };
+function renderResults(resultData) {
+  console.log("inside renderResults function");
+  console.log(resultData.data);
+  var commodityDate = document.createElement("h2");
+  commodityDate.textContent = resultData[0].date;
+  commodityContainer.append(commodityDate);
+}
 getCommodity();
-
